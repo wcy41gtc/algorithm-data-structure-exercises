9d7b3fc7d9d1be4971fe3424190a28363f6d6b94
Chaoyi Wang <wang140@llnl.gov>
2024-04-29 11:35:48 -0700

    modify sorted linked list

--- linked_list_practice/nested_linked_list.py
+++ linked_list_practice/nested_linked_list.py
@@ -61,6 +61,38 @@ class NestedLinkedList(LinkedList):
             return merge(node.value, None)
         return merge(node.value, self._flatten(node.next))
 
+class SortedLinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def append(self, value):
+        """
+        Append a value to the Linked List in ascending sorted order
+
+        Args:
+           value(int): Value to add to Linked List
+        """
+        
+        if self.head is None:
+            self.head = Node(value)
+            return
+        node = self.head
+        if value < self.head.value:
+            node = Node(value)
+            node.next = self.head
+            self.head = node
+            return
+        
+        node = self.head
+        while node.next is not None and value >= node.next.value:
+            node = node.next
+            
+        new_node = Node(value)
+        new_node.next = node.next
+        node.next = new_node
+        
+        return None
+
 def test():
     ''' Test merge() function'''
     linked_list = LinkedList(Node(1))
@@ -102,5 +134,17 @@ def test():
         #This will print 1 2 3 4 5
         print(node.value)
         node = node.next
+
+    # Test cases
+    linked_list = SortedLinkedList()
+    linked_list.append(3)
+    print ("Pass" if (linked_list.head.value == 3) else "Fail")
+
+    linked_list.append(2)
+    print ("Pass" if (linked_list.head.value == 2) else "Fail")
+
+    linked_list.append(4)
+    node = linked_list.head.next.next
+    print ("Pass" if (node.value == 4) else "Fail")
 if __name__ == '__main__':
     test()
