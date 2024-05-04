c30f58c31d91b8483b26451a4d0deabe6208a15a
Chaoyi Wang <wang140@llnl.gov>
2024-05-04 11:09:48 -0700

    add queue using high level python

---
+++ queue/queue_using_high_lvl_python.py
@@ -0,0 +1,38 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.size() == 0:
+            return None
+        return self.items.pop(0)
+    
+def test():
+    # Setup
+    q = Queue()
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+
+    # Test size
+    print ("Pass" if (q.size() == 3) else "Fail")
+
+    # Test dequeue
+    print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+    # Test enqueue
+    q.enqueue(4)
+    print ("Pass" if (q.dequeue() == 2) else "Fail")
+    print ("Pass" if (q.dequeue() == 3) else "Fail")
+    print ("Pass" if (q.dequeue() == 4) else "Fail")
+    q.enqueue(5)
+    print ("Pass" if (q.size() == 1) else "Fail")
+
+if __name__ == '__main__':
+    test()
