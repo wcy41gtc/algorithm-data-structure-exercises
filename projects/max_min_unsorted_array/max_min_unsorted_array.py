afd10f6adfe72025814e9e9af9102f72eb2737a1
Chaoyi Wang <wang140@llnl.gov>
2024-07-06 17:47:57 -0700

    add max min unsorted array

---
+++ projects/max_min_unsorted_array/max_min_unsorted_array.py
@@ -0,0 +1,33 @@
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+    
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    if len(ints) == 0:
+        raise ValueError("List must contain at least one integer")
+
+    # Initialize min and max to the first element
+    min_val = max_val = ints[0]
+
+    # Traverse the list from the second element onwards
+    for num in ints[1:]:
+        if num < min_val:
+            min_val = num
+        if num > max_val:
+            max_val = num
+
+    return (min_val, max_val)
+
+def test():
+    ### Example Test Case of Ten Integers
+    import random
+
+    l = [i for i in range(0, 10)]  # a list containing 0 - 9
+    random.shuffle(l)
+
+    print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
+if __name__ == "__main__":
+    test()
